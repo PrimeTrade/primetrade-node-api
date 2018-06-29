42954eee199252cbe1db43f69ae3ed06eda3b239
kunal-PrimeTrade <kunal@primetrade.ai>
2018-06-29 10:08:11 +0530

    fetching open orders for all exchages

--- index.js
+++ index.js
@@ -21,3 +21,24 @@ exports.getOpenOrders = (exchange_name, since, limit, public_apiKey,secret_key)=
     else
         console.log(`${exchange.id} doesn't have open orders to fetch`);
 };
+
+exports.getAllOpenOrders = (since, limit, public_apiKey, secret_key)=>{
+    _.each(ccxt.exchanges, (exchangeName)=>{
+        let exchange = new ccxt[exchangeName]({
+            apikey: public_apiKey,
+            secret: secret_key
+        });
+        if(exchange.has.fetchOpenOrders){
+            (async ()=>{
+                let markets = await exchange.loadMarkets();
+                _.each(markets,(val,symbol)=>{
+                    (async ()=>{
+                        return await exchange.fetchOpenOrders(symbol, since, limit);
+                    })();
+                })
+            })();
+        }
+        else
+            console.log(`${exchange.id} doesn't have open orders to fetch`);
+    })
+};
