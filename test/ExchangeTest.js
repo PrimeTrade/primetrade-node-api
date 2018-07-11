315cf3898933b208b7cab00e79118c6f56403dd2
kushagraPrimeTrade <kushagra@primetrade.ai>
2018-07-10 20:17:35 -0700

    added it function to the test file for testing

--- test/ExchangeTest.js
+++ test/ExchangeTest.js
@@ -1,23 +1,31 @@
 const expect = require('chai').expect;
 const nock = require('nock');
 
-const getExchanges = require('../index').getExchanges;
+const getExchange = require('../index').getExchange;
 const response = require('./Data/exchanges');
+
 
 describe('Get User tests', () => {
 
-  beforeEach(() => {
-    nock('http://api.primetrade.ai')
-      .get('/exchange')
-      .reply(200, response);
-  });
-    
-	it('Get the exchanges',() => {
-        return getExchange((data) => {})
-            .then(response => {
-                expect(response[0].equal('_1broker'));
-            });
+
+    beforeEach(() => {
+        nock('http://api.primetrade.ai').get('/exchange').reply(200, response);
     });
+
+
+    it('Get the exchanges',() => {
+        return getExchange()
+            .then(response => {
+            	expect(Array.isArray(response));
+                expect(typeof response).to.equal('object');
+                expect(response[0]).to.equal('_1broker');
+                expect(response[response.length-1]).to.equal('zb');
+
+            }
+        );
+    });
+
+
 });
 
 
