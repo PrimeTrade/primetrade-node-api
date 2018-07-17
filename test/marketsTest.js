cc84a8186c8d3b34343d551d1daf98cb5d18c442
kushagraPrimeTrade <kushagra@primetrade.ai>
2018-07-17 12:55:39 -0700

    updated the it function in markettest to test for the markets

--- test/marketsTest.js
+++ test/marketsTest.js
@@ -11,5 +11,18 @@ describe('Get User tests', () => {
         nock('http://api.primetrade.ai').get('/exchange/bittrex/markets').reply(200, response);
     });
 
+	it('Get the market Currencies',() => {
+
+        index.getMarketCurrency('bittrex', (data)=>{
+            response = data;
+        });
+
+        expect(typeof response).to.equal('object');
+        expect(response).to.not.be.empty;
+        expect(Array.isArray(response));
+
+
+    });
+	
 });
 
