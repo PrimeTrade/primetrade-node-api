3b850d74ad95a2ecc3df30e173fd10c75f93d1f1
Nainakshi <coder@primetrade.ai>
2018-07-19 14:00:09 -0700

    Modified

--- test/getInfoTest.js
+++ test/getInfoTest.js
@@ -4,11 +4,13 @@ const index = require('../index.js');
 let response = require('./Data/Info');
 describe('Get info test',() => {
     beforeEach(() => {
-        nock(`http://api.primetrade.ai`).get(`/exchange/bittrex`).reply(response);
+        nock(`http://api.primetrade.ai`).get(`/exchange/bittrex`).reply(200,response);//Correct exchangeName
+        nock(`http://api.primetrade.ai`).get(`/exchange/bitrex`).reply(404,response);//Incorrect exchangeName
     });
     it('Get info by exchangeName',() => {
         index.getInfo('bittrex',(data) => {
             response = data;
+
         });
         expect(typeof response).to.equal('object');
         expect (response).to.not.be.empty;
